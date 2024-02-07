7ab927b296ffe6680cfcd6f1e989760b69b5f0a3
Alexandria Roberts <iamlexieonearth@gmail.com>
2024-02-07 21:05:33 +0800

    Working on logout

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 
-interface RequestWithBdy extends Request {
+interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
 }
 
@@ -8,27 +8,52 @@ const router = Router();
 
 router.get('/login', (req: Request, res: Response) => {
   res.send(`
-  <form method="POST">
-   <div>
-     <label>Email</label>
-     <input name="email" />
-   </div>
-   <div>
-     <label>Password</label>
-     <input name="password" type="password"/>
-   </div>
-   <button>Submit</button>
-  </form>
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
   `);
 });
 
-router.post('/login', (req: RequestWithBdy, res: Response) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
-  if (email) {
-    res.send(email + password);
+
+  if (email && password && email === 'hi@hi.com' && password === 'password') {
+    req.session = { loggedIn: true };
+    res.redirect('/');
   } else {
-    res.send('You must provide an email');
+    res.send('Invalid email or password');
   }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+  }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = null;
+  res.redirect('/');
 });
 
 export { router };
