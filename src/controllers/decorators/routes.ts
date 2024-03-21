b24e4e1abd60b6aa1f051ccf1e124b357ab41c11
Alexandria Roberts <iamlexieonearth@gmail.com>
2024-03-21 22:40:02 +0800

    refactoring the routes and adding put, post routes

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,7 +1,15 @@
 import 'reflect-metadata';
 
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata('path', path, target, key);
+function routeBindr(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata('path', path, target, key);
+      Reflect.defineMetadata('method', method, target, key);
+    };
   };
 }
+
+export const get = routeBindr('get');
+export const put = routeBindr('put');
+export const post = routeBindr('post');
+export const del = routeBindr('delete');
