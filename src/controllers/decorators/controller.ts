062e02c5b1f6eafea52383f60ee23e9fb963e306
Alexandria Roberts <iamlexieonearth@gmail.com>
2024-04-09 22:32:46 +0800

    bodyValidator decorator

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -2,7 +2,11 @@ import 'reflect-metadata';
 import { AppRouter } from '../../AppRouter';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
+import { Request, Response, NextFunction, RequestHandler } from 'express';
 
+function bodyValidators(keys: string): RequestHandler {
+  return function (req: Request, res: Response, next: NextFunction) {};
+}
 export function controller(routePrefix: string) {
   return function (target: Function) {
     const router = AppRouter.getInstance();
